8eee5ef3dd20421cfaf3f181497fa657147c63ed
Paul Gasbarra <paulgasbarra@Admins-MacBook-Air.local>
2024-06-10 14:21:05 -0400

    fixing up broken pokedex

--- blank/src/components/Pokedex.jsx
+++ blank/src/components/Pokedex.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Display from "./Display";
 
 const Pokedex = () => {
-  const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonList, setPokemonList] = useState(["no data"]);
 
   return (
     <div className="container">
@@ -11,7 +11,9 @@ const Pokedex = () => {
       </div>
       <div className="pokemonList">
         <h3>Available Pokemon</h3>
-        <ul>{pokemonList.length > 1 && pokemonList.map((p) => <li />)}</ul>
+        <ul>
+          {pokemonList.length > 0 && pokemonList.map((p) => <li>{p}</li>)}
+        </ul>
       </div>
     </div>
   );
